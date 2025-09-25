1138aa23016f1ba62215b11c640196635ee19b83
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2025-09-25 11:01:33 +0530

    Added process.env.PORT

--- Server.js
+++ Server.js
@@ -1,9 +1,11 @@
 import express, { application } from 'express';
 import { generate } from './chatbot.js';
 import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express();
 
-const PORT = 3001;
+const PORT = 3001 || process.env.PORT;
 app.use(cors())
 app.use(express.json());
 
